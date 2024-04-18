dd0ec6223e97b7435f4794abe0e908934b79d46c
AnasOudadsse <135606982+AnasOudadsse@users.noreply.github.com>
2024-04-18 11:03:56 +0100

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -10,4 +10,3 @@ FLUSH PRIVILEGES;
 -- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
-
